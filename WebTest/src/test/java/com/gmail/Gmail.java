1e76075faad2c6a425a37bb163b194853b3a11c2
Biswajit <biswajit.biswal1@gmai.com>
2019-03-20 10:51:39 +0530

    second commit

---
+++ WebTest/src/test/java/com/gmail/Gmail.java
@@ -0,0 +1,19 @@
+package com.gmail;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Gmail {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
