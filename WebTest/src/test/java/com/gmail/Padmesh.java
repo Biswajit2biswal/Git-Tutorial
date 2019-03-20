416eecd16053bde5648bc132fc9978e0a3b8720e
Biswajit <biswajit.biswal1@gmai.com>
2019-03-20 10:53:27 +0530

    p message

---
+++ WebTest/src/test/java/com/gmail/Padmesh.java
@@ -0,0 +1,19 @@
+package com.gmail;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Padmesh {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
